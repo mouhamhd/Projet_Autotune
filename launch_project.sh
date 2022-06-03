92c5b1db08fca07af35b691b31cd1a4d0435129e
Robin Gerzaguet <robin.gerzaguet@irisa.fr>
2022-06-03 14:58:53 +0200

    Update path and gitignore

--- launch_project.sh
+++ launch_project.sh
@@ -5,5 +5,5 @@ source /usr/local/Vivado-2017.4/Vivado/2017.4/settings64.sh
 # Sourcing path to SDK
 source /usr/local/Vivado-2017.4/SDK/2017.4/settings64.sh 
 # Launch the SDK 
-xsdk --workspace ./Zybo_PTS_Application
+xsdk --workspace ./Zybo_DSP_Application/
 
