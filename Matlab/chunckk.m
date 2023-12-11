5f20f2f0e2c3df9e5c30e5b1a4a96f9f908ce46a
yate <mouha.yate@icloud.com>
2023-12-11 18:22:56 +0100

    a chunck signal 2

---
+++ Matlab/chunckk.m
@@ -0,0 +1,24 @@
+% ##########################
+% Projet PTS
+% 
+% Auteurs : KAFNDO Boenzemwendé Israel & YATE Mouhammeth
+% 
+% Enseignant : Robin GERZAGUET
+
+clc;
+close all;
+clear all
+
+function chunck (X , fs , size_fft , duration)
+
+Ts =  1/fs;
+p = duration/Ts;
+k = length(X)/p;
+
+
+for i = 1 : p : k
+    Mat = 
+end
+end
+
+
