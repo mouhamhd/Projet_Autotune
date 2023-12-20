ab064ce39d9bc84d6433fd95c94a01b608fc0a20
KAFANDO Boenzemwendé Israel <bkafando@enssat.fr>
2023-12-20 19:26:20 +0100

    FixSine_debug

--- Matlab/Test.m
+++ Matlab/Test.m
@@ -7,13 +7,42 @@
 
 addpath('./Functions/');
 addpath('.././Base_Sound/');
+% 
+% size_fft = 1024;
+% d = 0.01;
+% 
+% file ='/users/elo/bkafando/Bureau/Projet_PTS/mouhameth_israel_projet_pts/Base_Sound/synth_sweep_1.wav';
+% [x, fs] = analyse_audio(file);
+% Modulus = time_frequency(x, fs, size_fft, d);
+% active_frequency(Modulus);
 
-size_fft = 1024;
-d = 0.01;
 
-file ='/users/elo/myate/Projet_PTS_Israel_Mouhameth/2a_sysnum_dsp_project_autotune/Base_Sound/synth_sweep_1.wav';
-[x, fs] = analyse_audio(file);
-Modulus = time_frequency(x, fs, size_fft, d);
-active_frequency(Modulus);
+
+%**********************************************%
+
+a = 12/pi - 9/4;
+b = -2*a  + 5/2;
+c = a - 3/2;
+
+
+x = -1.5:0.01:1.5;
+y = zeros(1, length(x));
+y_p = y;
+for i = 1:length(x)
+    y(i) = fixSine(x(i));
+end
+
+for i = 1:length(x)
+    y_p(i) = a*x(i) + b*(x(i))^3 + c*(x(i))^5;
+end
+
+figure(1); hold on;
+plot(x, y/(max(abs(y))));
+figure(1); hold on;
+plot(x, sin((pi/2)*x));
+figure(1); hold on;
+plot(x, y_p);
+legend('sine','y[p]');
+legend('FixSine','sine','y[p]');
 
 %time_frequency (sig , fs , size_fft , d);
